ac8d3c156513c1d184d6752e6480aa8f770a0ddb
tasddc1226 <tasddc@naver.com>
2022-07-19 18:53:58 +0900

    Fix: code line formatting 변경으로 인한 수정

--- mydoctor/models.py
+++ mydoctor/models.py
@@ -33,28 +33,48 @@ class Doctor(models.Model):
         max_length=(16 * 11),
         verbose_name="진료과",
     )
-    non_paid = models.CharField(max_length=64, null=True, blank=True, verbose_name="비급여진료과목")
+    non_paid = models.CharField(
+        max_length=64, null=True, blank=True, verbose_name="비급여진료과목"
+    )
 
     class Meta:
         db_table = "doctor"
 
 
 class WeekDayTime(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, verbose_name="의사")
-    weekday_from = models.PositiveSmallIntegerField(choices=WEEKDAYS, verbose_name="영업 시작 요일")
-    weekday_to = models.PositiveSmallIntegerField(choices=WEEKDAYS, verbose_name="영업 종료 요일")
-    from_hour = models.PositiveSmallIntegerField(choices=HOUR_OF_DAY_24, verbose_name="영업 시작 시각")
-    to_hour = models.PositiveSmallIntegerField(choices=HOUR_OF_DAY_24, verbose_name="영업 종료 시각")
-    launch_from = models.PositiveSmallIntegerField(choices=HOUR_OF_DAY_24, verbose_name="점심 시작 시각")
-    launch_to = models.PositiveSmallIntegerField(choices=HOUR_OF_DAY_24, verbose_name="점심 종료 시각")
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, verbose_name="의사"
+    )
+    weekday_from = models.PositiveSmallIntegerField(
+        choices=WEEKDAYS, verbose_name="영업 시작 요일"
+    )
+    weekday_to = models.PositiveSmallIntegerField(
+        choices=WEEKDAYS, verbose_name="영업 종료 요일"
+    )
+    from_hour = models.PositiveSmallIntegerField(
+        choices=HOUR_OF_DAY_24, verbose_name="영업 시작 시각"
+    )
+    to_hour = models.PositiveSmallIntegerField(
+        choices=HOUR_OF_DAY_24, verbose_name="영업 종료 시각"
+    )
+    launch_from = models.PositiveSmallIntegerField(
+        choices=HOUR_OF_DAY_24, verbose_name="점심 시작 시각"
+    )
+    launch_to = models.PositiveSmallIntegerField(
+        choices=HOUR_OF_DAY_24, verbose_name="점심 종료 시각"
+    )
 
     class Meta:
         db_table = "weekday"
 
 
 class WeekendTime(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, verbose_name="의사")
-    holiday = models.PositiveSmallIntegerField(choices=WEEKENDS, verbose_name="주말 요일")
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, verbose_name="의사"
+    )
+    holiday = models.PositiveSmallIntegerField(
+        choices=WEEKENDS, verbose_name="주말 요일"
+    )
     closed = models.BooleanField(default=True, verbose_name="휴일 유무")
     from_hour = models.PositiveSmallIntegerField(
         choices=HOUR_OF_DAY_24, null=True, blank=True, verbose_name="영업 시작 시각"
