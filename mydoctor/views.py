4fd2216d2caf1e9a65abc9b29ff1998555974fb2
tasddc1226 <tasddc@naver.com>
2022-07-21 01:23:11 +0900

    Fix: TODO, 주석 추가 및 book_time 예외처리 추가

--- mydoctor/views.py
+++ mydoctor/views.py
@@ -91,6 +91,7 @@ class CareRequestView(ListAPIView):
         doctor = get_object_or_404(Doctor, id=d_id)
 
     def _convert_hour_list_to_datetime_object_list(self, hour_list, now):
+        # ex) [9, 12, 13, 18] -> [09:00, 12:00, 13:00, 18:00]
         res = []
         for hour in hour_list:
             res.append(
@@ -100,6 +101,7 @@ class CareRequestView(ListAPIView):
 
     def _set_conditions(self, datetime_objs_list, now):
         if len(datetime_objs_list) > 2:
+            # 평일 조건
             is_workday_weekday = (
                 datetime_objs_list[0] <= now < datetime_objs_list[1]
                 or datetime_objs_list[2] <= now < datetime_objs_list[3]
@@ -107,6 +109,7 @@ class CareRequestView(ListAPIView):
             is_lunch_time = datetime_objs_list[1] <= now < datetime_objs_list[2]
             is_workday_weekend = False
         else:
+            # 주말 조건
             is_workday_weekday = False
             is_workday_weekend = (
                 datetime_objs_list[0] <= now < datetime_objs_list[1]
@@ -119,6 +122,7 @@ class CareRequestView(ListAPIView):
         self._is_valid_request_ids(data["patient_id"], data["doctor_id"])
 
         ## 희망 예약 진료 시각에 대하여 해당 의사가 진료가 가능한지 확인
+        now = datetime.now()
         book_time = datetime(
             data["year"],
             data["month"],
@@ -126,6 +130,12 @@ class CareRequestView(ListAPIView):
             data["hour"],
             data["min"],
         )
+        if now > book_time:
+            return Response(
+                {"message": "진료 예약이 불가능합니다. 다른 시간을 선택해주세요."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         no = book_time.isoweekday()
 
         if no < 6:
@@ -148,8 +158,6 @@ class CareRequestView(ListAPIView):
 
         if objs:
             # 진료 예약이 가능하다면 요청 만료 시각 계산
-            # now = datetime.now()
-            now = datetime(2022, 1, 15, 1, 0)
             hour_range_list = list(objs.first())
             datetime_objs_list = (
                 self._convert_hour_list_to_datetime_object_list(
@@ -185,6 +193,7 @@ class CareRequestView(ListAPIView):
                     }
                 else:
                     # 금일 영업시간 종료로 인한 부재
+                    # TODO: 주말에 영업을 하지 않는 의사에 대한 다음 영업일 찾기
                     info = {
                         "patient": data["patient_id"],
                         "doctor": data["doctor_id"],
