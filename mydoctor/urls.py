64cabd428d045d7db2a71f298c4bea93117f7b0c
tasddc1226 <tasddc@naver.com>
2022-07-22 15:17:51 +0900

    Fix: end point url 수정

--- mydoctor/urls.py
+++ mydoctor/urls.py
@@ -4,8 +4,8 @@ from .views import DoctorSeaerchView, CareRequestView
 
 urlpatterns = [
     path("search/", DoctorSeaerchView.as_view(), name="search doctor"),
-    path("care/", CareRequestView.as_view(), name="care request"),
+    path("cares/", CareRequestView.as_view(), name="care request"),
     path(
-        "care/<int:id>", CareRequestView.as_view(), name="care request accept"
+        "cares/<int:id>", CareRequestView.as_view(), name="care request accept"
     ),
 ]
