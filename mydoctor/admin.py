c770536153e50d0b51e069ab6183421e0756c593
tasddc1226 <tasddc@naver.com>
2022-07-22 15:18:19 +0900

    Fix: 불필요한 model 등록 제거

--- mydoctor/admin.py
+++ mydoctor/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Patient, Doctor, WeekDayTime, WeekendTime, CareRequestList
+from .models import Patient, Doctor, WeekDayTime, WeekendTime
 
 # Register your models here.
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(WeekDayTime)
 admin.site.register(WeekendTime)
-admin.site.register(CareRequestList)
